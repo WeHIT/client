ef55c1a3e01514fae1844da88f674d1451a0c93f
rccoder <rccoder.net@gmail.com>
2017-03-12 16:01:12 +0800

    feat: Optimize speakFlow

--- src/pages/home/container/SpeakFlow/index.js
+++ src/pages/home/container/SpeakFlow/index.js
@@ -15,63 +15,38 @@ import MultiImgText from '../../component/MultiImgText';
 export default class SpeakFlow extends Component {
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <View style={styles.lineView}>
-          <EasySpeak 
-            pos='right'
-            text='今天天气怎么样'
-          />
-        </View>
-        <View style={styles.lineView}>
-          <EasySpeak
-            pos='left'
-            text='你所在地区是哈尔滨，今天温度 -40 度，注意身体别冻死自己'
-          />
-        </View>
-        <View style={styles.lineView}>
-          <MultiImgText />
-        </View>
-        <View style={styles.lineView}>
-          <MultiImgText />
-        </View>       
-      </ScrollView>
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.lineView}>
+            <EasySpeak
+              pos='right'
+              text='今天天气怎么样'
+            />
+          </View>
+          <View style={styles.lineView}>
+            <EasySpeak
+              pos='left'
+              text='你所在地区是哈尔滨，今天温度 -40 度，注意身体别冻死自己'
+            />
+          </View>
+          <View style={styles.lineView}>
+            <MultiImgText />
+          </View>
+          <View style={styles.lineView}>
+            <MultiImgText />
+          </View>
+        </ScrollView>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 20,
-    height: Dimensions.get('window').height - 200
+    paddingTop: 0,
+    height: Dimensions.get('window').height - 140
   },
   lineView: {
-    marginBottom: 20
+    marginTop: 20
   },
-  viewTextWhite: {
-    width: 300,
-    backgroundColor: 'white',
-    marginTop: 20,
-    marginLeft: 10,
-    paddingTop: 12,
-    paddingBottom: 12,
-    paddingLeft: 8,
-    paddingRight: 8,
-    borderTopLeftRadius: 16,
-    borderBottomRightRadius: 16
-  },
-  viewTextGreen: {
-    width: 300,
-    backgroundColor: '#35C878',
-    marginTop: 20,
-    marginLeft: 64,
-    paddingTop: 12,
-    paddingBottom: 12,
-    paddingLeft: 8,
-    paddingRight: 8,
-    borderTopRightRadius: 16,
-    borderBottomLeftRadius: 16    
-  },
-  textStyle: {
-    lineHeight: 20
-  }
 });
