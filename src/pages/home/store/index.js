befd88e603b5ec46e6f0aac8ce2765c4064ba48b
rccoder <rccoder.net@gmail.com>
2017-04-04 21:47:43 +0800

    feat: use react-native remote redux debugger

--- src/pages/home/store/index.js
+++ src/pages/home/store/index.js
@@ -1,7 +1,10 @@
-import { createStore, applyMiddleware, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "../reducer";
+import devTools from 'remote-redux-devtools';
+
+import { createStore, applyMiddleware, compose } from "redux";
 import { routerMiddleware } from "react-router-redux";
+import { Platform } from 'react-native';
 
 export default function configureStore(initialState, history) {
   const middleware = [thunkMiddleware, routerMiddleware(history)];
@@ -10,7 +13,11 @@ export default function configureStore(initialState, history) {
     initialState,
     compose(
       applyMiddleware(...middleware),
-      window.devToolsExtension ? window.devToolsExtension() : f => f
+      devTools({
+        name: Platform.OS,
+        hostname: '127.0.0.1',
+        port: 5678
+      }),
     )
   );
 
